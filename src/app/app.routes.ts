4d530d49fbdf6c1b58756a99dd7e357fa47f5489
Lolerw <angelus.lolerw@gmail.com>
2025-07-16 22:16:53 -0500

    Se agrego navbar persistente vision Desktop y Mobile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { CatalogoComponent } from './Presentacion/Componentes/catalogo/catalogo.component';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './Presentacion/Componentes/nosotros/nosotros.component';
 
 export const routes: Routes = [
-    {
-        path: 'catalogo',
-        component: CatalogoComponent,
-      }
-];
+    {path: 'catalogo',component: CatalogoComponent,},
+    {path: 'home', component: HomeComponent},
+    {path: 'nosotros', component: NosotrosComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
+  ];
