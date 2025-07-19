c30b139c83579e677ab48635fcd8cc19e33eee84
Lolerw <angelus.lolerw@gmail.com>
2025-07-18 22:26:04 -0500

    Apartado  Nosotros y Canales (falta responsividad)

--- src/app/Presentacion/Componentes/nosotros/nosotros.component.ts
+++ src/app/Presentacion/Componentes/nosotros/nosotros.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { ENLACES } from '../../../config/enlaces.config';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { CanalesComponent } from '../canales/canales.component';
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [],
+  imports: [CanalesComponent],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
 export class NosotrosComponent {
+  idNosotros = ENLACES.idVideoNosotrosYT;
+  videLocalNosotos = '/assets/VIDEO_PRUEBA.mp4'
+  videoNosotros: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer){
+    const id = this.idNosotros;
+    const embedUrl = `https://www.youtube.com/embed/${id}?autoplay=1&mute=1&loop=1&playlist=${id}`;
+    this.videoNosotros = this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+  }
 }
---
+++ src/app/config/enlaces.config.ts
@@ -0,0 +1,7 @@
+export const ENLACES = {
+    DriveVideo: 'https://drive.google.com/file/d/1l25t6mix1j17DlaUb-kLOPy_To06YTxJ/preview',
+    YoutubeVideo: 'https://youtu.be/Q7wuxhS-_QM',
+    idVideoNosotrosYT: 'Q7wuxhS-_QM',
+    idVideoNosotros: '1l25t6mix1j17DlaUb-kLOPy_To06YTxJ',
+    fireBaseStorage:'https://firebasestorage.googleapis.com/v0/b/mediatest-b5789.firebasestorage.app/o/VIDEO_PRUEBA.mp4?alt=media&token='
+};
