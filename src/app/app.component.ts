f12be388d10bb492256f17309eeea176ff8054a4
cristell-0821 <N00236512@upn.pe>
2025-07-22 23:54:13 -0500

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { CatalogoComponent } from './Presentacion/Componentes/catalogo/catalogo.
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './Presentacion/Componentes/navbar/navbar.component';
 import { MobilNavBarComponent } from './Presentacion/Componentes/mobil-nav-bar/mobil-nav-bar.component';
+import { FooterComponent } from './Presentacion/Componentes/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CatalogoComponent, HomeComponent, NavbarComponent, MobilNavBarComponent],
+  imports: [RouterOutlet,CatalogoComponent, HomeComponent, NavbarComponent, MobilNavBarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
